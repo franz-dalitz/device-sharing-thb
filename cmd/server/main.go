892ad7014a03fd80bf89bef28b32038766632aaf
Alina Holz <alina.holz@bcxp.de>
2025-06-17 11:08:05 +0200

    Fix azure docker problems

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,6 +9,7 @@ func main() {
     if port == "" {
         port = "8080" // fallback for local development
     }
+    log.Println("Listening on port:", port)
 	engine := internal.Server()
 	internal.Db.Mock()
 	engine.Run(":" + port)
